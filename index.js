7774947fb253ac32a1756e4378d2b26c9bd371c0
Michal Jach <michal.jach@ig.com>
2019-04-01 15:53:11 +0200

    Tests

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let events = {};
-let toAppend = {};
+const events = {};
+const toAppend = {};
 
-HTMLElement.prototype._addEventListener = HTMLElement.prototype.addEventListener;
-HTMLElement.prototype.addEventListener = function(event, handler){
-  this._addEventListener(event, handler); 
-  if(!this.listeners) { this.listeners = [] }
+HTMLElement.prototype.addEventListenerProxy = HTMLElement.prototype.addEventListener;
+HTMLElement.prototype.addEventListener = function listener(event, handler) {
+  this.addEventListenerProxy(event, handler);
+  if (!this.listeners) { this.listeners = []; }
   this.listeners.push({ event, handler });
 };
 
@@ -12,9 +12,23 @@ const createListener = (element, attribute, action) => {
   const handler = element.getAttribute(attribute);
   if (handler) {
     element.removeAttribute(attribute);
-    element.addEventListener(action, (e) => events[handler].call(e.target, e));
+    element.addEventListener(action, e => events[handler].call(e.target, e));
   }
-}
+};
+
+const cloneNode = (node) => {
+  const clone = node.cloneNode(false);
+  if (node.listeners) {
+    node.listeners.forEach((listener) => {
+      clone.addEventListener(listener.event, e => listener.handler.call(e.target, e));
+    });
+  }
+
+  if (node.childNodes.length) {
+    Array.from(node.childNodes).forEach(elem => clone.appendChild(cloneNode(elem)));
+  }
+  return clone;
+};
 
 const appendNodes = (element) => {
   const id = element.getAttribute('data-append');
@@ -22,35 +36,19 @@ const appendNodes = (element) => {
   if (id) {
     element.removeAttribute('data-append');
     if (Array.isArray(toAppend[id])) {
-      toAppend[id].forEach(elem => {
+      toAppend[id].forEach((elem) => {
         if (typeof elem === 'string') {
           element.append(elem);
         } else {
           element.appendChild(cloneNode(elem));
         }
-      })
+      });
     } else {
       element.appendChild(toAppend[id]);
     }
     delete toAppend[id];
   }
-}
-
-const cloneNode = (node) => {
-  const clone = node.cloneNode(false);
-  if (node.listeners) {
-    node.listeners.forEach(listener => {
-      clone.addEventListener(listener.event, (e) => listener.handler.call(e.target, e));
-    })
-  }
-
-  if (node.childNodes.length) {
-    Array.from(node.childNodes).forEach(elem => {
-      clone.appendChild(cloneNode(elem));
-    })
-  }
-  return clone;
-}
+};
 
 const attachEvents = (nodes) => {
   const wrapper = nodes[0].parentElement.tagName === 'BODY' ? document.createElement('div') : nodes[0].parentElement;
@@ -59,6 +57,7 @@ const attachEvents = (nodes) => {
     if (element.getAttribute) {
       createListener(element, 'onClick', 'click');
       createListener(element, 'onKeyPress', 'keypress');
+      createListener(element, 'onKeyUp', 'keyup');
       appendNodes(element);
     }
 
@@ -70,11 +69,11 @@ const attachEvents = (nodes) => {
   });
 
   return wrapper;
-}
+};
 
-export const hts = (strings, ...props) => {
+export default (strings, ...props) => {
   const string = strings.map((partial, i) => {
-    let id = Date.now() + i;
+    const id = Date.now() + i;
     switch (typeof props[i]) {
       case 'function':
         events[id] = props[i];
@@ -86,7 +85,7 @@ export const hts = (strings, ...props) => {
         return `${partial}${props[i] || ''}`;
     }
   }).join('');
-  
+
   const nodes = new DOMParser().parseFromString(string, 'text/html').body.childNodes;
   return attachEvents(nodes);
-}
+};
---
+++ test/index.test.js
@@ -0,0 +1,92 @@
+import hts from '../index';
+
+test('renders text correctly', () => {
+  expect(hts`test`.innerHTML).toEqual('test');
+});
+
+test('renders property correctly', () => {
+  const name = 'Me';
+  expect(hts`${name}`.innerHTML).toEqual('Me');
+});
+
+test('fires button event correctly', () => {
+  const handler = jest.fn();
+  const elem = hts`<button onclick=${handler}>Button</button>`;
+  elem.firstElementChild.click();
+  expect(elem.firstElementChild.listeners.length).toEqual(1);
+  expect(handler).toHaveBeenCalledTimes(1);
+  expect(handler).toHaveBeenCalledWith(new MouseEvent({}));
+});
+
+test('fires button event correctly with params', () => {
+  const handler = jest.fn();
+  const elem = hts`<button onclick=${handler.bind(this, 'foo')}>Button</button>`;
+  elem.firstElementChild.click();
+  expect(elem.firstElementChild.listeners.length).toEqual(1);
+  expect(handler).toHaveBeenCalledTimes(1);
+  expect(handler).toHaveBeenCalledWith('foo', new MouseEvent({}));
+});
+
+test('fires input multiple event correctly with params', () => {
+  const handler = jest.fn();
+  const elem = hts`<input onkeypress=${handler.bind(this, 'press')} onkeyup=${handler.bind(this, 'up')}>`;
+  const keyboardEvent = document.createEvent('KeyboardEvent');
+  const keyboardEvent2 = document.createEvent('KeyboardEvent');
+  const initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+
+  keyboardEvent[initMethod](
+    'keypress',
+    true,
+    true,
+    window,
+    false,
+    false,
+    false,
+    false,
+    40,
+    0,
+  );
+  elem.firstElementChild.dispatchEvent(keyboardEvent);
+
+  keyboardEvent2[initMethod](
+    'keyup',
+    true,
+    true,
+    window,
+    false,
+    false,
+    false,
+    false,
+    40,
+    0,
+  );
+  elem.firstElementChild.dispatchEvent(keyboardEvent2);
+
+  expect(elem.firstElementChild.listeners.length).toEqual(2);
+  expect(handler).toHaveBeenCalledTimes(2);
+  expect(handler).toHaveBeenCalledWith('press', new KeyboardEvent({}));
+  expect(handler).toHaveBeenCalledWith('up', new KeyboardEvent({}));
+});
+
+test('renders nested element correctly', () => {
+  const elem = hts`elem`;
+  expect(hts`${elem}`.innerHTML).toEqual('<div><div>elem</div></div>');
+});
+
+test('renders array correctly', () => {
+  const data = ['foo', 'bar'];
+  expect(hts`${data}`.innerHTML).toEqual('<div>foobar</div>');
+});
+
+test('renders loop of elements correctly', () => {
+  const elem = hts`elem`;
+  expect(hts`${[1, 2].map(() => elem)}`.innerHTML).toEqual('<div><div>elem</div><div>elem</div></div>');
+});
+
+test('renders loop of elements with events correctly', () => {
+  const handler = jest.fn();
+  const elem = hts`<button onclick=${handler}>Button</button>`;
+  elem.firstElementChild.click();
+  expect(hts`${[1, 2].map(() => elem)}`.innerHTML).toEqual('<div><div><button>Button</button></div><div><button>Button</button></div></div>');
+  expect(handler).toHaveBeenCalledTimes(1);
+});
