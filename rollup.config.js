f412b5e78546329598d3897bf7ee9829ca5afb89
Michal Jach <michal.jach@ig.com>
2019-04-08 15:05:39 +0200

    1.0.10

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,17 @@ export default [
     input: 'src/index.js',
     output: {
       format: 'es',
+      file: 'dist/htsx.esm.min.js',
+      name: 'htsx',
+    },
+    plugins: [
+      minify(),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      format: 'iife',
       file: 'dist/htsx.min.js',
       name: 'htsx',
     },
